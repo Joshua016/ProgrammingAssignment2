d4b0df671e5be573bc5bd0be30c21893ac026c6a
Joshua016 <91407350+Joshua016@users.noreply.github.com>
2021-09-27 11:53:24 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,12 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
 ## input x is used as a matrix
 ## calculate inverse matrix
 ## set the value "s" as null
+
 library(MASS)
 makeCacheMatrix <- function(x = matrix()){
   inv <- NULL
